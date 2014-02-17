064d8af1ce120e837d702616eaaaa9859e3c7675
John Tyree <johntyree@gmail.com>
2014-02-17 12:55:24 -0600

    ENH: Add entry point

--- setup.py
+++ setup.py
@@ -8,8 +8,6 @@ with open('README.md')as f:
 
 setup(name='bldr',
       version='0.0.1',
-      packages=['bldr'],
-      scripts=['bin/bldr'],
       author='John Tyree',
       author_email='johntyree@gmail.com',
       license='LGPL3+',
@@ -17,10 +15,14 @@ setup(name='bldr',
       description="Helper for building simple programs",
       keywords="build compile",
       long_description=long_desc,
+      py_modules=['bldr'],
+      entry_points={
+          'console_scripts': ['bldr = bldr.bldr:main'],
+          },
       classifiers=[
           "Development Status :: 3 - Alpha",
           "License :: OSI Approved :: "
           "GNU Lesser General Public License v3 or later (LGPLv3+)",
-          "Topic :: Utilities",  #FIXME
-      ],
+          "Topic :: Utilities",  # FIXME
+          ],
       )
